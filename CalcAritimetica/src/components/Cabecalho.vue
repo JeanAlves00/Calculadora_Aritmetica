988e910aaa1161ed1ff12ed767b139b29ef86bf4
Jean <154983968+JeanAlves00@users.noreply.github.com>
2024-10-16 09:56:52 -0400

    termino

---
+++ CalcAritimetica/src/components/Cabecalho.vue
@@ -0,0 +1,9 @@
+<template>
+    <header class="p-5 mb-4 mt-5 bg-dark rounded-4 text-center text-white shadow-lg">
+        <div class="container">
+            <h1 class="display-4 fw-bold">Calculadora Aritmética</h1>
+            <hr class="my-4 border-light" />
+        </div>
+    </header>
+</template>
+  
---
+++ CalcAritimetica/src/components/Rodape.vue
@@ -0,0 +1,58 @@
+<template>
+    <footer class="text-center mt-5 animate-fade-in">
+        <div>
+            <ul class="list-unstyled d-flex justify-content-center align-items-center fs-3 mt-4 mb-3">
+                <li class="mx-3">
+                    <a href="https://www.linkedin.com/in/jean-pablo-alves-gon%C3%A7alves-a82477212/" target="_blank">
+                    <i class="bi bi-linkedin vibrant-text"></i>
+                    </a>
+                </li>
+                <li class="mx-3">
+                    <a href="https://github.com/JeanAlves00" target="_blank">
+                    <i class="bi bi-github vibrant-text"></i>
+                    </a>
+                </li>
+            </ul>
+            <p class="text-black mb-3 vibrant-text">
+            &copy; {{ new Date().getFullYear() }} - Jean Pablo Alves Gonçalves - Todos os direitos reservados.
+            </p>
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+
+    .vibrant-text {
+        color: #ff5733;
+        transition: color 0.3s ease, transform 0.3s ease;
+    }
+  
+    .vibrant-text:hover {
+        color: #000000;
+        
+    }
+
+    @keyframes fadeIn {
+        0% {
+            opacity: 0;
+            transform: translateY(20px);
+        }
+        100% {
+            opacity: 1;
+            transform: translateY(0);
+        }
+    }
+  
+    .animate-fade-in {
+        animation: fadeIn 1s ease-in-out;
+    }   
+  
+    a {
+        text-decoration: none;
+    }
+  
+    li:hover {
+    transform: scale(1.2);
+    }
+</style>
+  
